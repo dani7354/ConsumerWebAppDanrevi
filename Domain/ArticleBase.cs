ef796df22cceaede448ae507a4bec2720ec37294
Daniel Stuhr Petersen <dani7354@edu.eal.dk>
2019-05-01 17:43:49 +0200

    ArticleBase

--- Domain/ArticleBase.cs
+++ Domain/ArticleBase.cs
@@ -4,12 +4,11 @@ using System.Text;
 
 namespace Domain
 {
-    public class Article
+    public abstract class ArticleBase
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime Created { get; set; }
-        public string TagString { get; set; }
     }
 }
