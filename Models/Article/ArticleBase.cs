403b6f2e13a2201b105003b97d3ad872d811b349
Daniel Stuhr Petersen <dani7354@edu.eal.dk>
2019-05-02 16:02:43 +0200

    added model for creating articles

--- Models/Article/ArticleBase.cs
+++ Models/Article/ArticleBase.cs
@@ -7,14 +7,12 @@ namespace Models.Article
 {
     public abstract class ArticleBase
     {
-        [JsonProperty("id")]
-        public int Id { get; set; }
+
         [JsonProperty("title")]
         public string Title { get; set; }
         [JsonProperty("content")]
         public string Content { get; set; }
-        [JsonProperty("date_created")]
-        public DateTime DateCreated { get; set; }
+
 
     }
 }
--- Models/Article/ArticleDetailsViewModel.cs
+++ Models/Article/ArticleDetailsViewModel.cs
@@ -7,9 +7,14 @@ namespace Models.Article
 {
     public class ArticleDetailsViewModel : ArticleBase
     {
+        [JsonProperty("id")]
+        public int Id { get; set; }
         [JsonProperty("author")]
         public Author Author { get; set; }
         [JsonProperty("tags")]
         public List<string> Tags { get; set; }
+
+        [JsonProperty("date_created")]
+        public DateTime DateCreated { get; set; }
     }
 }
