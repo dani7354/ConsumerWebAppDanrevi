f38d04b34816501dc1b365be7daab993b9454f09
dani7354 <dani7354@edu.eal.dk>
2019-05-08 14:21:01 +0200

    controller and model for login

---
+++ Models/Login/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Models.Login
+{
+    public class LoginViewModel
+    {
+      [JsonProperty("email")]
+       public string Email { get; set; }
+      [JsonProperty("password")]
+       public string Password { get; set; }
+    }
+}
