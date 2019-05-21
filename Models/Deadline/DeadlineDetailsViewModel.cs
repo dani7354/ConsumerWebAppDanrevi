0964a42e882c151d92c474b0bca06a85d45a8230
dani7354 <dani7354@edu.eal.dk>
2019-05-21 10:04:49 +0200

    Added model and view for Deadlines

---
+++ Models/Deadline/DeadlineDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Models.Deadline
+{
+    public class DeadlineDetailsViewModel
+    {
+      public int Id { get; set; }
+      public string Name { get; set; }
+      public string Description { get; set; }
+      public DateTime Date { get; set; }
+
+    }
+}
